1afe216ca8c24d4af4765fafb079c54c5d9e0590
Sam Steele <sam.steele@gmail.com>
2005-06-11 18:18:27 +0000

    lobby
    git-svn-id: svn+ssh://c99.org/home/c99org/svn/DCSquares/trunk@42 8944af1b-4df6-0310-90e0-d6e02aab4a67

--- include/net.h
+++ include/net.h
@@ -11,4 +11,19 @@ void net_update();
 void netplay_init();
 void net_sendpacket(char *packet);
 void process_game_packet(char *buf);
+void process_packet(char *msg);
+void process_server_packet(int msgid, char*data);
+void process_chat_packet(int msgid, char*data);
 int lobby_connect(char *host, char *username, char *password);
+void lobby_update();
+struct userlist_node {
+	char username[50];
+	struct userlist_node *next;
+};
+
+struct userlist_node *get_userlist();
+void add_user(char *username);
+int userlist_size();
+
+void os_chat_insert_text(char *text);
+void os_chat_reload_users();
