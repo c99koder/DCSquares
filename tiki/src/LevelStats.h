df429df5f51a545e37ba7b6645cb41e8e2499430
Sam Steele <sam.steele@gmail.com>
2006-01-14 21:12:31 +0000

    Initial import of LevelStats
    git-svn-id: svn+ssh://c99.org/home/c99org/svn/DCSquares/trunk@113 8944af1b-4df6-0310-90e0-d6e02aab4a67

---
+++ tiki/src/LevelStats.h
@@ -0,0 +1,26 @@
+/*
+ *  LevelStats.h
+ *  TikiSquares
+ *
+ *  Created by Sam Steele on 1/1/06.
+ *  Copyright 2006 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+#include <Tiki/genmenu.h>
+#include "drawables/playField.h"
+#include "drawables/squaresBg.h"
+#include "drawables/banna.h"
+#include "drawables/squaresHUD.h"
+#include "DCSMenu.h"
+
+class LevelStats : public DCSMenu {
+public:
+	LevelStats();
+	void init();
+	
+protected:
+		void inputEvent(const Event & evt);
+private:
+	RefPtr<squaresHUD> hud;
+};
